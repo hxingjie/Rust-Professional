ae8392cac7c2b1c8e5e70f053d806533b5140b75
ubuntu <ubuntu@localhost.localdomain>
2025-02-13 19:10:50 +0800

    update

--- exercises/hard/solutiont4/src/calc_time.rs
+++ exercises/hard/solutiont4/src/calc_time.rs
@@ -1,6 +1,4 @@
-use std::collect::{HashMap, HashSet};
-
-const MONTH2DAYS: [u32; 13] = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+use std::collections::{HashMap, HashSet};
 
 fn is_leapyear(year: u32) -> bool {
     (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
@@ -16,44 +14,64 @@ fn str_to_num(s: &str) -> u32 {
 }
 
 fn date_to_week_day(year: u32, month: u32, day: u32) -> u32 {
-    let mut ans = 0;
-    let j = (year / 100) as i32;
-    let k = (year % 100) as i32;
-    let month = if month == 1 || month == 2 {
-        month + 12
+    // 根据日期计算当天是周几
+    let c = (year / 100) as i32; // 20
+    let mut y = (year % 100) as i32; // 25
+    let mut m: i32 = 0;
+    let d: i32 = day as i32;
+    if month == 1 || month == 2 {
+        m = month as i32 + 12;
+        y -= 1;
     } else {
-        month
+        m = month as i32;
     };
-    let h = (k + k/4 + j/4 - 2*j + (26*(month as i32+1)/10 + day as i32 - 1)) % 7;
-    if (h+6) % 7 == 0 {
-        ans = 7;
-    } else {
-        ans = (h as u32 + 6) % 7;
+    let mut h = (y + y/4 + c/4 - 2*c + (13*(m as i32 + 1)/5 + d - 1)) % 7;
+    if h == 0 {
+        h = 7;
     }
 
-    ans
+    h as u32
 }
 
 fn date_to_week_count(year: u32, month: u32, day: u32) -> u32 {
-    let week_day = date_to_week_day(year, 1, 1);
-    let mut ans = (day + week_day - 1 ) / 7;
-    if ans == 0 {
-        ans = 1;
+    let mut week_day = date_to_week_day(year, 1, 1); // 该年的1月1日是周几
+    if week_day == 7 {
+        week_day = 0;
+    }
+    let (days, _) = count_days(year, month, day); // 计算该年已经过了多少天
+    let mut ans = 0;
+    if week_day > 4 {
+        if day <= 7-week_day+1 { // 是否是去年的最后一周
+            ans = date_to_week_count(year-1, 12, 31);
+        } else {
+            ans = (days + week_day - 3) / 7 + 1;
+        }
+    } else {
+        ans = (days + week_day - 1) / 7 + 1;
     }
 
-    ans
+    if month == 12 {
+        let cur_year_last_week_day = date_to_week_day(year, 12, 31);
+        if cur_year_last_week_day < 4 && 31-day < cur_year_last_week_day { // 是否是明年的第一周
+            1
+        } else {
+            ans
+        }
+    } else {
+        ans
+    }
 }
 
 fn count_days(year: u32, month: u32, day: u32) -> (u32, u32) {
     let mut ans_0 = 0;
     let mut ans_1 = 0;
+    let mut month2days: [u32; 13] = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if is_leapyear(year) {
+        month2days[2] = 29;
+    }
     let is_leapyear = is_leapyear(year);
     for m in 1..month {
-        if is_leapyear && m == 2 {
-            ans_0 += MONTH2DAYS[m as usize] + 1;
-        } else {
-            ans_0 += MONTH2DAYS[m as usize];
-        }
+        ans_0 += month2days[m as usize];
     }
     ans_0 += day;
 
@@ -70,9 +88,11 @@ fn compute_diff_of_date(year0: u32, month0: u32, day0: u32, year1: u32, month1: 
     if year0 == year1 {
         let (days_0, _) = count_days(year0, month0, day0);
         let (days_1, _) = count_days(year1, month1, day1);
-        days_0 - days_1 - 1
+        days_0 - days_1
     } else {
-        0
+        let (days_0, _) = count_days(year0, month0, day0);
+        let (_, days_1) = count_days(year1, month1, day1);
+        days_0 + days_1
     }
 }
 
@@ -89,27 +109,47 @@ fn date_to_newyear(year: u32, month: u32, day: u32, date2newyear: HashMap<u32, (
         year, month, day)
 }
 
-fn date_to_trading_days(year: u32, month: u32, day: u32) -> u32 {
-    // let mut date2holiday: HashSet<(u32, u32)> = HashSet::new();
-    // date2holiday.insert((10, 1));
-    // date2holiday.insert((10, 2));
-    // date2holiday.insert((10, 3));
-    // date2holiday.insert((10, 4));
-    // date2holiday.insert((10, 5));
-    // date2holiday.insert((10, 6));
-    // date2holiday.insert((10, 7));
-
-    // if date2holiday.contains(&(month, day)) {
-    //     return 0;
-    // }
-
-    let week_day = date_to_week_day(year, month, day);
-    match week_day {
-        1..=5 => 0,
-        6 => 1,
-        7 => 0,
-        _ => panic!("date_to_trading_days error"),
+fn next_day(year: u32, month: u32, day: u32) -> (u32, u32, u32) {
+    let mut month2days: [u32; 13] = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if is_leapyear(year) {
+        month2days[2] = 29;
     }
+    if month2days[month as usize] == day { // 该月的最后一天
+        if month == 12 { // 该年的最后一天
+            (year+1, 1, 1)
+        } else {
+            (year, month+1, 1)
+        }
+    } else {
+        (year, month, day+1)
+    }
+}
+
+fn is_holiday(year: u32, month: u32, day: u32) -> bool {
+    let mut holidays: HashSet<(u32, u32)> = HashSet::new();
+    holidays.insert((1, 1));
+    holidays.insert((5, 1));
+    holidays.insert((5, 2));
+    holidays.insert((5, 3));
+    holidays.insert((5, 4));
+    //holidays.insert((5, 5));
+    if holidays.contains(&(month, day)) {
+        true
+    } else {
+        let week_day = date_to_week_day(year, month, day);
+        match week_day {
+            1..=5 => false,
+            _ => true,
+        }
+    }
+}
+
+fn date_to_trading_days(year: u32, month: u32, day: u32) -> u32 {
+    let mut trading_day = next_day(year, month, day);
+    while is_holiday(trading_day.0, trading_day.1, trading_day.2) {
+        trading_day = next_day(trading_day.0, trading_day.1, trading_day.2);
+    }
+    compute_diff_of_date(trading_day.0, trading_day.1, trading_day.2, year, month, day) - 1
 }
 
 pub fn time_info(time: &str) -> String {
@@ -125,7 +165,7 @@ pub fn time_info(time: &str) -> String {
 
     let mut date2newyear = HashMap::new();
     date2newyear.insert(2025, (1, 29));
-    date2newyear.insert(2026, (1, 19));
+    date2newyear.insert(2026, (2, 17));
     let days_2 = date_to_newyear(year, month, day, date2newyear);
     
     let days_3 = date_to_trading_days(year, month, day);
